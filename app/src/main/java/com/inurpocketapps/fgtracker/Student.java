0cb2ac5ee4bc781ad932ab674c63411a081f7d62
jonathanmccrite <jonathanrmccrite@gmail.com>
2019-11-14 16:08:06 -0600

    Added basic functionality for the Student class

--- app/src/main/java/com/inurpocketapps/fgtracker/Student.java
+++ app/src/main/java/com/inurpocketapps/fgtracker/Student.java
@@ -1,6 +1,43 @@
 package com.inurpocketapps.fgtracker;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Student {
+    private Map<String, Integer> testList;
 
+    //Default constructor
+    //all it needs to do for now is initialize testList
+    public Student() {
+        testList = new HashMap<String, Integer>();
+    }
 
+    public Map<String, Integer> getTestList() {
+        return testList;
+    }
+
+    public void setTest(Map<String, Integer> testList) {
+        this.testList = testList;
+    }
+
+    //add data for a test, should create a new field for the test
+    //if it does not already have a field in the map
+    public void addTest(String testKey, int testValue) {
+        testList.put(testKey, testValue);
+    }
+
+    //add a field to the map for a test without providing any results
+    public void addTest(String testKey) {
+        testList.put(testKey, 0);
+    }
+
+    //get test data from the map, requires a valid key
+    public int getTest(String test) {
+        Integer data = testList.get(test);
+        if(data == null) {
+            return 0;
+        }
+        else {
+            return data;
+        }
+    }
 }
