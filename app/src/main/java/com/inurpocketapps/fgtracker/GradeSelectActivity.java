bb6f9e57208c5de101b6fb5b406350f97634a877
ingramRob <43839545+ingramRob@users.noreply.github.com>
2019-11-23 13:26:46 -0800

    Minor changes... still crashes

--- app/src/main/java/com/inurpocketapps/fgtracker/GradeSelectActivity.java
+++ app/src/main/java/com/inurpocketapps/fgtracker/GradeSelectActivity.java
@@ -6,14 +6,12 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -73,44 +71,33 @@ public class GradeSelectActivity extends AppCompatActivity implements AddGradeDi
         Log.i("FIREBASE", "Loaded schoolDoc");
         getSchool();
 
-        // Populate the list of grades
-        initializeGradeList();
-
         // Set View Adapter
         adapt = new GradeAdapter(grades, this);
         resView.setAdapter(adapt);
 
         // Toolbar properties
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //todo: add code for grade adder popup
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //todo: add code for grade adder popup
+//            }
+//        });
     }
 
 
     private void getSchool() {
-        //DocumentReference schoolDoc = schoolCollection.document(skl);
-//        schoolDoc.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//            @Override
-//            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                School sk = documentSnapshot.toObject(School.class);
-//                school = sk;
-//            }
-//        });
         schoolDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 Log.e("GET_SCHOOL", "onComplete is Called");
                 if (task.isSuccessful()) {
                     DocumentSnapshot doc = task.getResult();
-                    School sk = doc.toObject(School.class);
-                    school = sk;
+                    school = doc.toObject(School.class);
+                    initializeGradeList();
                 }
             }
         });
@@ -120,16 +107,19 @@ public class GradeSelectActivity extends AppCompatActivity implements AddGradeDi
                 Log.e("GET_SCHOOL", "Failed Hard");
             }
         });
-
     }
 
     private void initializeGradeList() {
         Log.i("GRADE_LIST", "Starting to Initialize Grade List");
-        grades = school.getGrades();
+        if (school.getGrades() != null)
+        {
+            school.getGrades();
+        }
     }
 
     // On Click Method for the add button
     public void addGrade(View view) {
+        // Create and alert dialog for the Grade input
         AddGradeDialog grdDialog = new AddGradeDialog();
         grdDialog.show(getSupportFragmentManager(), "newGrade");
     }
